514963d1e627303e39a985896820996e55849771
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-31 17:03:28 +0300

    task 58 is ready

--- Program.cs
+++ Program.cs
@@ -6,19 +6,6 @@
 // 18 20
 // 15 18
 
-// int [,] GetMatrix(int m, int n)
-// {
-//     int [,] matrix = new int[m,n];
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matrix [i,j] = new Random().Next(0,10);
-//         }
-//     }
-//     return matrix;
-// }
-
 void PrintMatrix(int [,] InputMatrix)
 {
     for (int i = 0; i < InputMatrix.GetLength(0); i++)
