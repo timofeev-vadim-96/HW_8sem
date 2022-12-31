f47ddc295ac0bc1e28b58a88591d654613b5883c
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-31 17:02:22 +0300

    task 58

--- Program.cs
+++ Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// int [,] GetMatrix(int m, int n)
+// {
+//     int [,] matrix = new int[m,n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix [i,j] = new Random().Next(0,10);
+//         }
+//     }
+//     return matrix;
+// }
+
+void PrintMatrix(int [,] InputMatrix)
+{
+    for (int i = 0; i < InputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < InputMatrix.GetLength(1); j++)
+        {
+            System.Console.Write(InputMatrix[i,j] + "\t");
+        }    
+        System.Console.WriteLine();    
+    }
+}
+
+int [,] ProductOfMatrices (int [,] FirstMatrix, int [,] SecondMatrix)
+{
+    int [,] ResultMatrix = new int [FirstMatrix.GetLength(0), FirstMatrix.GetLength(1)];
+    for (int i = 0; i < ResultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < ResultMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < ResultMatrix.GetLength(0); k++)
+            {
+                ResultMatrix[i,j] += (FirstMatrix[i,k] * SecondMatrix[k,j]) ;
+            }
+        }
+    }
+    return ResultMatrix; 
+}
+
+int [,] matrix1 = new int [,] {{2,4},{3,2}};
+int [,] matrix2 = new int [,] {{3,4},{3,3}};
+System.Console.WriteLine("Первая матрица: ");
+PrintMatrix(matrix1);
+
+System.Console.WriteLine("Вторая матрица: ");
+PrintMatrix(matrix2);
+System.Console.WriteLine();
+
+System.Console.WriteLine("Результирующая матрица: ");
+int [,] resMatr = ProductOfMatrices(matrix1,matrix2);
+PrintMatrix(resMatr);
+
+
+
+
+
+
